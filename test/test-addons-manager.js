077489893eda9d77016063fed7de4db7b34b300f
James Hugman <james@hugman.tv>
2015-09-14 01:08:46 +0100

    Add-on manager: loading and configuring an addon with a manifest.

--- test/test-addons-manager.js
+++ test/test-addons-manager.js
@@ -1,17 +1,38 @@
 'use strict';
-let test = require('tap').test
+let test = require('tap').test,
+    path = require('path')
 
-let registry = require('../lib/registry')
-let Registry = registry.Registry, 
-    createAddons = registry.createAddons
+
+let Registry = require('../lib/registry').Registry, 
+    AddonManager = require('../lib/addons-manager').AddonManager
 
 test('manifest loaded, maybe', (t) => {
   let registry = new Registry()
-  // let addons = createAddons(registry)
-  // addons.load('test-addon-1/manifest.json')
-  //   .then((addon) => {
-  //     t.ok(addon, 'Something came back') 
-  //     t.end()
-  //   })
-  t.end()
+  let addons = new AddonManager(registry, path.join(__dirname, 'fixtures'))
+  //t.ok(addons)
+
+  let loadAddon = addons.load('./hello-world')
+
+  t.ok(loadAddon, 'loading an addon returns something')
+  t.ok(loadAddon.then, 'it looks like a promise')
+
+  loadAddon
+    .then((addon) => {
+      t.ok(addon, 'addon exists')
+      t.ok(addon.hasOwnProperty('notAvailable'), 'addon is allowed notAvailable, even if it is not available')
+      t.notOk(addon.hasOwnProperty('notAllowed'), 'addon has no access to a singleton not requested in the manifest')
+      
+      t.ok(addon.hasOwnProperty('epTester'), 'addon has epTester allowed')
+      t.equal(addon.epTester.prop1, 'yes', 'addon has epTester from manifest')
+      
+      t.throws(() => {
+        // has asked for permission, but no singleton was available
+        let singleton = addon.notAvailable
+      })
+      t.end()
+    })
+    .catch((err) => {
+      console.log('ERROR ', err)
+      t.fail()
+    })
 })
